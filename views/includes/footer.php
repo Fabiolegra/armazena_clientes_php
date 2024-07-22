e67b3af21f8dd24b456632e422d9ced5e788ddc3
Fabiolegra <fabioargel3@gmail.com>
2024-07-22 14:48:10 -0300

    feat: header e footer adicionados

---
+++ views/includes/footer.php
@@ -0,0 +1,32 @@
+</div>
+</div>
+</main>
+<footer class="page-footer" style="background-color: rgb(2 6 23);">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Feito em php</h5>
+                <p class="grey-text text-lighten-4">com materialize e tailwind banco de dados postgers</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="https://fabioargel.onrender.com" target="_blank">portifolio</a></li>
+                    <li><a class="grey-text text-lighten-3" href="https://github.com/Fabiolegra" target="_blank">Github</a></li>
+                    <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/fabio-argel-30802a232" target="_blank">Linkendln</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2024 Nocopyright
+        </div>
+    </div>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+
+
+</html>
---
+++ views/includes/header.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel=" stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <style>
+        h1,
+        h2,
+        p,
+        input {
+            color: white !important;
+        }
+
+        h1 {
+            font-size: 3rem !important;
+        }
+
+        td,
+        th {
+            text-align: center !important;
+        }
+
+        nav {
+            position: sticky;
+            top: 0;
+            z-index: 2;
+        }
+    </style>
+</head>
+
+<body style="background-color: rgb(30 41 59);">
+    <nav class="" style="background-color: rgb(2 6 23);">
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Fabio Argel</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="../adicionar.php"><i class="material-icons left">add_circle</i>Adicionar Cliente</a></li>
+                <li><a href="../../index.php"><i class="material-icons left">visibility</i>Ver Clientes</a></li>
+            </ul>
+        </div>
+
+    </nav>
+    <main class="" style="min-height: 100vh;">
+        <div class='row'>
+            <div class="col s12 m6 push-m3">
